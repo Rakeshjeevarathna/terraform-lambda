b2e4b057152e740f2b8e50b85ea085b7a920301c
Rakeshjeevarathna <81945595+Rakeshjeevarathna@users.noreply.github.com>
2021-04-09 10:11:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "myfunc" {
   filename         = "lambda.py.zip"
-  function_name    = "mylambdafunction"
+  function_name    = "mylambdafunction3"
   handler          = "lambda.lambdaHandler"
   source_code_hash = filebase64sha256("lambda.py.zip")
   role             = aws_iam_role.lambdarole.arn
@@ -35,7 +35,7 @@ resource "aws_s3_bucket" "s3bucket" {
 }
 
 resource "aws_cloudwatch_log_group" "cwlogs" {
-  name              = "/aws/lambda/mylambdafunction"
+  name              = "/aws/lambda/mylambdafunction3"
   retention_in_days = 14
 }
 
